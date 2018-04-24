8573dfa46360c6d24b280e74b77e2b33d0c6fb57
Alexis <alexclaustro@gmail.com>
2018-04-23 19:42:27 -0500

    Added Exit button,added on click events to new activity

--- src/main/java/com/alexis/tictactoe/MainActivity.java
+++ src/main/java/com/alexis/tictactoe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.alexis.tictactoe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPlayButton(View view){
+        Intent gotoSetupIntent =  new Intent(this, setupGame.class);
+        this.startActivity(gotoSetupIntent);
+    }
+
+    public void onExitBtn(View view){
+        System.exit(1);
     }
 }
---
+++ src/main/java/com/alexis/tictactoe/setupGame.java
@@ -0,0 +1,17 @@
+package com.alexis.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alexis on 23-Apr-18.
+ */
+
+public class setupGame extends Activity {
+
+    @Override
+    protected  void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.setup);
+    }
+}
